8bc6d3e59c02c02f79fe9c2c15e2f3944671020f
Raven Ravener <ravener.anime@gmail.com>
2026-03-01 19:11:54 +0500

    help command

---
+++ src/commands/general/help.ts
@@ -0,0 +1,38 @@
+import { type CommandConfig } from '#lib/command';
+import { commands } from '#lib/command-handler';
+import { Colors, OwnerId } from '#lib/constants';
+import { EmbedBuilder, type Message } from '@fluxerjs/core';
+
+export const config: CommandConfig = {
+    description: 'View bot commands',
+    aliases: ['commands', 'cmds', 'info']
+};
+
+export async function run(message: Message, args: string[]) {
+    const { user } = message.client;
+    const embed = new EmbedBuilder()
+        .setColor(Colors.Primary)
+        .setAuthor({ name: user!.username, iconURL: user!.displayAvatarURL() })
+        .setTitle('Hibiki - Commands');
+
+    const categories: Record<string, string[]> = {};
+
+    for (const command of commands.values()) {
+        const { config } = command;
+        if (!config.category) continue;
+        const category = categories[config.category] ?? (categories[config.category] = []);
+        category.push(config.name!);
+    }
+
+    if (message.author.id !== OwnerId) {
+        delete categories['Owner'];
+    }
+
+    const lines = [];
+    for (const [category, commands] of Object.entries(categories)) {
+        lines.push(`**${category}**: ${commands.map(c => `\`${c}\``).join(', ')}`);
+    }
+
+    embed.setDescription(lines.join('\n'));
+    await message.reply({ embeds: [embed] });
+}
