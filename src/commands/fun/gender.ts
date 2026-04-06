d65e46e3ba7495216e8e378f7fa180a1a42f35d6
Raven Ravener <ravener.anime@gmail.com>
2026-04-06 15:35:51 +0500

    fix

--- src/commands/fun/gender.ts
+++ src/commands/fun/gender.ts
@@ -11,14 +11,15 @@ interface Gender {
 
 export const config: CommandConfig = {
     description: 'Estimate someone\'s gender based on their name',
-    extendedHelp: 'This command uses the Genderize API to predict the gender of a person based on their name.\nThis is only provided for entertainment purposes.'
+    extendedHelp: 'This command uses the Genderize API to predict the gender of a person based on their name.\nThis is only provided for entertainment purposes.',
+    aliases: ['genderize']
 };
 
 export async function run(message: Message, args: string[]) {
     // parse mention -> if not mention, use as is -> if nothing provided use author
     const raw = args.join(' ');
     const targetId = parseUserMention(raw);
-    const target = targetId ? await message.client.users.fetch(targetId).then(u => u.globalName ?? u.username) : (raw ?? message.author);
+    const target = targetId ? await message.client.users.fetch(targetId).then(u => u.globalName ?? u.username) : (raw ?? message.author.globalName ?? message.author.username);
 
     const response = await fetch(`https://api.genderize.io?name=${encodeURIComponent(target)}`);
     if (!response.ok) {
