b3da06fcbf7326183e51448c7b8be58b2e9ce599
Raven Ravener <ravener.anime@gmail.com>
2026-05-08 13:48:51 +0500

    handle user links in chat

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,6 +1,31 @@
 import { handleCommands } from '#lib/command-handler';
+import { buildOsuProfileEmbed } from '#lib/embeds';
+import { api } from '#lib/osu';
 import type { Client, Message } from '@fluxerjs/core';
+import { Ruleset } from 'osu-api-v2-js';
+
+const regex = /^http(?:s)?:\/\/osu.ppy.sh\/users\/([^\/\s]+)(?:\/(?:osu|fruits|taiko|mania)(?:\/)?)?/;
+async function handleUserLink(message: Message) {
+    const match = message.content.match(regex);
+    if (!match) return;
+
+    const userId = match[1]!;
+    const gameMode = match[2] ? match[2].toLowerCase() : 'osu';
+
+    const rulesetMap: Record<string, Ruleset> = {
+        osu: Ruleset.osu,
+        fruits: Ruleset.fruits,
+        taiko: Ruleset.taiko,
+        mania: Ruleset.mania
+    };
+
+    const user = await api.getUser(!isNaN(parseInt(userId)) ? parseInt(userId) : userId, rulesetMap[gameMode] || Ruleset.osu);
+    await message.reply({
+        embeds: [buildOsuProfileEmbed(user)]
+    });
+}
 
 export async function run(client: Client, message: Message) {
     await handleCommands(message);
+    await handleUserLink(message);
 }
